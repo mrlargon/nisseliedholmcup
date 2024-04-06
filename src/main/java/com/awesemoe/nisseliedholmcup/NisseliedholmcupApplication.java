0b8eb0c82f8e1d12e458e706a09fb05e57c6e85e
mrlargon <mr_largon@hotmail.com>
2024-04-06 17:42:30 +0200

    first

---
+++ src/main/java/com/awesemoe/nisseliedholmcup/NisseliedholmcupApplication.java
@@ -0,0 +1,13 @@
+package com.awesemoe.nisseliedholmcup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NisseliedholmcupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NisseliedholmcupApplication.class, args);
+    }
+
+}
